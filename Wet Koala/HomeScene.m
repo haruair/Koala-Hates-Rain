0c4d5171666969cb03c08dd8180ecaa0a6f199f2
haruair <onward.edkim@gmail.com>
2014-02-16 00:04:39 +1100

    add home scene sprite

--- Wet Koala/HomeScene.m
+++ Wet Koala/HomeScene.m
@@ -7,7 +7,84 @@
 //
 
 #import "HomeScene.h"
+#import "GameScene.h"
+
+#import "ButtonNode.h"
 
 @implementation HomeScene
+
+-(id)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        
+        self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+
+        SKTextureAtlas * atlas = [SKTextureAtlas atlasNamed:@"sprite"];
+        
+        SKSpriteNode * background = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"background"]];
+        background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+        [self addChild:background];
+        
+        SKSpriteNode * title = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"text-logo"]];
+        title.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame) * 5 / 8);
+        [self addChild:title];
+        
+        [title runAction:
+         [SKAction repeatActionForever:
+          [SKAction sequence:@[
+                               [SKAction moveByX:0 y:-5 duration:0.3],
+                               [SKAction moveByX:0 y:5 duration:0.3]
+                               ]
+           ]
+          ]
+         ];
+        
+        SKSpriteNode * copyright = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"text-copyright"]];
+        copyright.position = CGPointMake(self.size.width / 2, self.size.height / 4 - 60);
+        [self addChild:copyright];
+        
+        
+        CGFloat buttonY = CGRectGetMidY(self.frame) / 2;
+        
+        SKTexture * startDefault = [atlas textureNamed:@"button-start-off"];
+        SKTexture * startTouched = [atlas textureNamed:@"button-start-on"];
+        
+        ButtonNode * startButton = [[ButtonNode alloc] initWithDefaultTexture:startDefault andTouchedTexture:startTouched];
+        startButton.position = CGPointMake(CGRectGetMidX(self.frame) - (startButton.size.width / 2 + 10), buttonY);
+        
+        [startButton setMethod: ^ (void) { [self startButtonPressed]; } ];
+        [self addChild:startButton];
+        
+        SKTexture * scoreDefault = [atlas textureNamed:@"button-score-off"];
+        SKTexture * scoreTouched = [atlas textureNamed:@"button-score-on"];
+        
+        ButtonNode * scoreButton = [[ButtonNode alloc] initWithDefaultTexture:scoreDefault andTouchedTexture:scoreTouched];
+        scoreButton.position = CGPointMake(CGRectGetMidX(self.frame) + (scoreButton.size.width / 2 + 10), buttonY);
+        
+        [scoreButton setMethod: ^ (void) { [self scoreButtonPressed]; } ];
+        [self addChild:scoreButton];
+
+    }
+    return self;
+}
+
+-(void) startButtonPressed {
+}
+
+-(void) scoreButtonPressed {
+    
+}
+
+
+-(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [ButtonNode doButtonsActionEnded:self touches:touches withEvent:event];
+}
+
+-(void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+}
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [ButtonNode doButtonsActionBegan:self touches:touches withEvent:event];
+}
+
 
 @end
